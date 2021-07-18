c135649248ad0ec1ce4f52b6ec03bca84a9de80e
Haf <haf@protonmail.ch>
2021-07-18 17:52:51 +0200

    activation script to systemd unit

--- osticket/osticket.nix
+++ osticket/osticket.nix
@@ -5,15 +5,16 @@ let
   user = "osticket";
 in
   {
-
     environment.systemPackages = with pkgs; [
       # I'm using these two just for testing on nixos-shell
       php74 
       vim
     ];
 
-    # TODO: Make these systemd units with more explicit ordering?
-    system.activationScripts.prepare = ''
+    # TODO: Creating the directory wouldn't be necessary if createHome were working
+    # is it that it's badly ordered?
+    # I can't think of any solution to the problem of setting the executable bit
+    system.activationScripts.createDirectory = ''
         echo "CREATING DIRECTORIES AND SETTING PERMISSIONS..."
         mkdir -p ${directory}
         dirsUpToPath=$(namei ${directory} | tail -n +3 | cut -d' ' -f3)
@@ -24,21 +25,6 @@ in
           currentPath="$currentPath/$dir"
           chmod og+x "$currentPath"
         done
-
-        echo "COPYING TO DESTINATION..."
-        if [ ! "$(ls -A ${directory})" ]; then
-          echo ">>> ${directory} is empty, copying osTicket files there"
-          cp -r ${osTicket}/* ${directory}
-          find ${directory} -type d -print0 | xargs -0 chmod 0755
-          find ${directory} -type f -print0 | xargs -0 chmod 0644
-          mv ${directory}/include/ost-sampleconfig.php ${directory}/include/ost-config.php
-          chmod 0666 ${directory}/include/ost-config.php
-        else
-          echo ">>> ${directory} not empty, considering already deployed"
-        fi
-
-        echo "MAKING USER OWN EVERYTHING"
-        chown -R ${user}:${user} ${directory}
     '';
 
     services.mysql = {
@@ -70,19 +56,19 @@ in
         root = directory;
         locations."/".extraConfig = ''
             index index.php;
-          '';
+        '';
         locations."~ \.php$".extraConfig = ''
             fastcgi_split_path_info ^(.+\.php)(/.+)$;
             fastcgi_pass unix:${config.services.phpfpm.pools.osTicket.socket};
             include ${pkgs.nginx}/conf/fastcgi_params;
             include ${pkgs.nginx}/conf/fastcgi.conf;
-          '';
+        '';
         locations."~ ^/scp/ajax.php/.*$".extraConfig = ''
             try_files $uri $uri/ /scp/ajax.php?$query_string;
-          '';
+        '';
         locations."/scp".extraConfig = ''
             try_files $uri $uri/ /scp/index.php;
-          '';
+        '';
       };
     };
 
@@ -111,39 +97,64 @@ in
       };
     };
 
-    systemd.services.install-osticket = {
-      script = ''
-        set -x
-        echo ">>> Installing osTicket"
-        ${pkgs.curl}/bin/curl "localhost/setup/install.php" \
-          -F "s=install" \
-          -F "name=Site Name" \
-          -F "email=sitemail@example.org" \
-          -F "fname=AdminFirstname" \
-          -F "lname=AdnminLastname" \
-          -F "admin_email=adminemail@example.org" \
-          -F "username=adminuser" \
-          -F "passwd=adminpass" \
-          -F "passwd2=adminpass" \
-          -F "prefix=ost_" \
-          -F "dbhost=localhost" \
-          -F "dbname=osticket" \
-          -F "dbuser=osticket" \
-          -F "dbpass=password"
+    systemd.services = {
+      deploy-osticket = {
+        script = ''
+          echo "COPYING TO DESTINATION..."
+          if [ ! "$(ls -A ${directory})" ]; then
+            echo ">>> ${directory} is empty, copying osTicket files there"
+            cp -r ${osTicket}/* ${directory}
+            find ${directory} -type d -print0 | xargs -0 chmod 0755
+            find ${directory} -type f -print0 | xargs -0 chmod 0644
+            mv ${directory}/include/ost-sampleconfig.php ${directory}/include/ost-config.php
+            chmod 0666 ${directory}/include/ost-config.php
+          else
+            echo ">>> ${directory} not empty, considering already deployed"
+          fi
+        '';
+
+        wantedBy = [ "multi-user.target" ];
+
+        serviceConfig = {
+          User = user;
+          Type = "oneshot";
+        };
+      };
+
+      install-osticket = {
+        script = ''
+          set -x
+          echo ">>> Installing osTicket"
+          ${pkgs.curl}/bin/curl "localhost/setup/install.php" \
+            -F "s=install" \
+            -F "name=Site Name" \
+            -F "email=sitemail@example.org" \
+            -F "fname=AdminFirstname" \
+            -F "lname=AdnminLastname" \
+            -F "admin_email=adminemail@example.org" \
+            -F "username=adminuser" \
+            -F "passwd=adminpass" \
+            -F "passwd2=adminpass" \
+            -F "prefix=ost_" \
+            -F "dbhost=localhost" \
+            -F "dbname=osticket" \
+            -F "dbuser=osticket" \
+            -F "dbpass=password"
           echo ">>> Performing post-install cleanup"
           chmod 0644 ${directory}/include/ost-config.php
           rm -r ${directory}/setup
-      '';
+        '';
 
-      wantedBy = [ "multi-user.target" ];
+        wantedBy = [ "multi-user.target" ];
 
-      unitConfig = {
-        After = [ "nginx.service" "phpfpm-osTicket.service" "mysql.service" ]; # any nix way to get the service names? especially phpfpm since it's more likely to change
-      };
+        unitConfig = {
+          After = [ "nginx.service" "phpfpm-osTicket.service" "mysql.service" "deploy-osticket.service" ]; # any nix way to get the service names? especially phpfpm since it's more likely to change
+        };
 
-      serviceConfig = {
-        User = user;
-        Type = "oneshot";
+        serviceConfig = {
+          User = user;
+          Type = "oneshot";
+        };
       };
     };
   }
