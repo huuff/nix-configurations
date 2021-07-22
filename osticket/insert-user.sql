6d42c6c0190308055acb1e25b428739ecf45e4ed
Haf <haf@protonmail.ch>
2021-07-22 19:00:25 +0200

    managed to get a working query to insert an user into db

--- osticket/insert-user.sql
+++ osticket/insert-user.sql
@@ -1,5 +1,8 @@
-START TRANSACTION;
-INSERT INTO ost_user_email;
-INSERT INTO ost_user_account;
-INSERT INTO ost_user;
-COMMIT;
+start transaction;
+insert into ost_user (org_id, default_email_id, name, created, updated) values (0, 0, "Testman", now(), now());
+select last_insert_id() into @user_id;
+insert into ost_user_email (user_id, address) values (last_insert_id(), "test@example.org");
+select last_insert_id() into @email_id;
+update ost_user set default_email_id=@email_id where id=@user_id;
+insert into ost_user_account (user_id, status, passwd) values (@user_id, 1, <htpasswd hash>);
+commit;
