630f5a71b6d8914730c9c78cf5af141e1057b977
Haf <haf@protonmail.ch>
2021-07-24 19:59:49 +0200

    flake for distributing neuron module

--- flake.nix
+++ flake.nix
@@ -6,26 +6,17 @@
     nixops.url = "github:NixOS/nixops";
     utils.url = "github:numtide/flake-utils";
     neuron.url = "github:srid/neuron";
-    mydrvs.url = "github:huuff/derivations";
+    myDrvs.url = "github:huuff/derivations";
   };
 
-  outputs = { self, nixpkgs, nixops, neuron, utils, mydrvs, ... }:
+  outputs = { self, nixpkgs, nixops, neuron, utils, myDrvs, ... }:
   {
 
     overlay = final: prev: {
       neuron-notes = neuron.packages.x86_64-linux.neuron;
     };
 
-    nixosConfigurations.neuron = { config, pkgs, repo, keyPath, ... }:
-    {
-      nixpkgs.overlays = [ self.overlay ];
-      imports = [
-        (import ./neuron/neuron.nix { inherit config pkgs repo keyPath; })
-        ./neuron/cachix.nix
-        mydrvs.nixosModules.do-on-request 
-        mydrvs.nixosModules.neuron-module
-      ];
-    };
+    nixosModules.neuron = import ./neuron/neuron.nix;
   };
 
 }
--- neuron/neuron.nix
+++ neuron/neuron.nix
@@ -37,66 +37,95 @@ in
         type = oneOf [ str path ];
         description = "Path to the SSH key that will allow pulling the repository";
       };
+
+      package = mkOption {
+        type = package;
+        default = pkgs.neuron-notes;
+        description = "Neuron package used to generate zettelkasten";
+      };
     };
 
     config = mkIf cfg.enable {
-    networking.firewall = {
-      allowedTCPPorts = [ 80 cfg.refreshPort ];
-    };
+      imports = [
+        ./cachix.nix
+        (builtins.fetchgit {
+          url = "https://github.com/huuff/derivations";
+          rev = "82a25a7bac0cee4a86144185741ed7390bad2f43"; 
+        }).nixosModules.do-on-request
+      ];
 
-    system.activationScripts = {
-      createDir = ''
-        echo ">>> Removing previous ${cfg.directory}"
-        rm -rf ${cfg.directory}/{,.[!.],..?}* # weird but it will delete hidden files too without returning an error for . and ..
-        echo ">>> Cloning ${cfg.repo} to ${cfg.directory}"
-        ${gitWithDeployKey} clone "${cfg.repo}" ${cfg.directory} 
-        echo ">>> Making ${cfg.user} own ${cfg.directory}"
-        chown -R ${cfg.user}:${cfg.user} ${cfg.directory}
+      networking.firewall = {
+        allowedTCPPorts = [ 80 cfg.refreshPort ];
+      };
+
+      system.activationScripts = {
+        createDir = ''
+          echo ">>> Removing previous ${cfg.directory}"
+          rm -rf ${cfg.directory}/{,.[!.],..?}* # weird but it will delete hidden files too without returning an error for . and ..
+          echo ">>> Cloning ${cfg.repo} to ${cfg.directory}"
+          ${gitWithDeployKey} clone "${cfg.repo}" ${cfg.directory} 
+          echo ">>> Making ${cfg.user} own ${cfg.directory}"
+          chown -R ${cfg.user}:${cfg.user} ${cfg.directory}
         '';
-    };
+      };
 
-    users.users.${cfg.user} = {
-      isSystemUser = true;
-      home = "${cfg.directory}";
-      group = cfg.user;
-      extraGroups = [ "keys" ]; # needed so it can access /run/keys
-      createHome = true;
-    };
-
-    users.groups.${cfg.user} = {};
-
-    systemd.services.nginx.serviceConfig.ProtectHome = "read-only";
-
-    services = {
-      nginx = {
-        enable = true;
-        user = cfg.user;
+      users.users.${cfg.user} = {
+        isSystemUser = true;
+        home = "${cfg.directory}";
         group = cfg.user;
+        extraGroups = [ "keys" ]; # needed so it can access /run/keys
+        createHome = true;
+      };
 
-        virtualHosts.neuron = {
-          enableACME = false;
-          root = "${cfg.directory}/.neuron/output";
-          locations."/".extraConfig = ''
+      users.groups.${cfg.user} = {};
+
+      systemd.services.nginx.serviceConfig.ProtectHome = "read-only";
+
+      services = {
+        nginx = {
+          enable = true;
+          user = cfg.user;
+          group = cfg.user;
+
+          virtualHosts.neuron = {
+            enableACME = false;
+            root = "${cfg.directory}/.neuron/output";
+            locations."/".extraConfig = ''
               index index.html index.htm;
             '';
+          };
+        };
+
+        do-on-request = {
+          user = cfg.user;
+          enable = true;
+          port = cfg.refreshPort;
+          workingDirectory = "${cfg.directory}";
+          script = ''
+            ${gitWithDeployKey} pull
+          '';
+        };
+
+        neuron = {
+          user = cfg.user;
+          enable = true;
+          path = "${cfg.directory}";
         };
       };
 
-      do-on-request = {
-        user = cfg.user;
-        enable = true;
-        port = cfg.refreshPort;
-        workingDirectory = "${cfg.directory}";
-        script = ''
-          ${gitWithDeployKey} pull
-        '';
+      systemd.services.neuron = {
+        description = "Watch and generate Neuron zettelkasten";
+
+        serviceConfig = {
+          User = cfg.user;
+          Restart = "on-failure";
+          WorkingDirectory = cfg.directory;
+          ExecStart = "${cfg.package}/bin/neuron rib -w";
+        };
+
+        wantedBy = [ "default.target" ];
+
       };
 
-      neuron = {
-        user = cfg.user;
-        enable = true;
-        path = "${cfg.directory}";
-      };
     };
-  };
   }
