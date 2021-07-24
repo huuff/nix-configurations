190de88f5ab61ce9f8ce0f418237b0f1cb8d504b
Haf <haf@protonmail.ch>
2021-07-24 14:37:55 +0200

    changed db password to filename

--- osticket/example/mycli.sh
+++ osticket/example/mycli.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-mycli --ssh-host localhost --ssh-port 2222 --ssh-user root --ssh-password pass -P 3306 -u osticket -ppassword -D osticket
+mycli --ssh-host localhost --ssh-port 2222 --ssh-user root --ssh-password pass -P 3306 -u osticket -pdbpass -D osticket
