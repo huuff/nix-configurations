141fb62f5c9aacbfe9b75c20b2cb78e64d856e58
Haf <haf@protonmail.ch>
2021-07-24 19:31:57 +0200

    made neuron a module instead of a function

--- TODO.md
+++ TODO.md
@@ -5,7 +5,6 @@
 ## Neuron
 * Add SSL options
 * Add the password option since I'm using it in my personal instance and doesn't seem like a bad idea
-* Make it a module instead of a function
 
 ## osTicket
 * Still can't enter localhost:8989/scp (Or more explicitly: it doesn't redirect me to scp/admin.php)
--- neuron/neuron.nix
+++ neuron/neuron.nix
@@ -1,67 +1,99 @@
-{ config, pkgs, repo, keyPath, actionPort ? 55000, directory ? "/home/neuron", user ? "neuron", ... }:
+{ config, pkgs, ... }:
 let
+  cfg = config.services.neuron;
   gitWithDeployKey = ''${pkgs.git}/bin/git -c 'core.sshCommand=${pkgs.openssh}/bin/ssh -i ${keyPath} -o StrictHostKeyChecking=no' '';
 in
   {
+    options.services.neuron = with types; {
+      enable = mkEnableOption "Automatically fetch Neuron zettelkasten from git repo and serve it";
+
+      refreshPort = mkOption {
+        type = int;
+        default = 55000;
+        description = "Sending a request to this port will trigger a git pull to refresh the zettelkasten from a repo.";
+      };
+
+      directory = mkOption {
+        type = oneOf [ str path ];
+        default = "/home/neuron";
+        description = "Directory from which to serve the zettelkasten";
+      };
+
+      user = mkOption {
+        type = str;
+        default = "neuron";
+        description = "User that will save and serve Neuron";
+      };
+
+      repository = mkOption {
+        type = str;
+        description = "Repository that holds the zettelkasten";
+      };
+
+      deployKey = mkOption {
+        type = oneOf [ str path ];
+        description = "Path to the SSH key that will allow pulling the repository";
+      };
+    };
+
+    config = mkIf cfg.enable {
     networking.firewall = {
-      allowedTCPPorts = [ 80 actionPort];
+      allowedTCPPorts = [ 80 cfg.refreshPort ];
     };
 
     system.activationScripts = {
       createDir = ''
-        echo "START ACTIVATION SCRIPT"
-        echo "Removing previous ${directory}"
-        rm -rf ${directory}/{,.[!.],..?}* # weird but it will delete hidden files too without returning an error for . and ..
-        echo "Cloning ${repo} to ${directory}"
-        ${gitWithDeployKey} clone "${repo}" ${directory} 
-        echo "Making ${user} own ${directory}"
-        chown -R ${user}:${user} ${directory}
+        echo ">>> Removing previous ${cfg.directory}"
+        rm -rf ${cfg.directory}/{,.[!.],..?}* # weird but it will delete hidden files too without returning an error for . and ..
+        echo ">>> Cloning ${cfg.repo} to ${cfg.directory}"
+        ${gitWithDeployKey} clone "${cfg.repo}" ${cfg.directory} 
+        echo ">>> Making ${cfg.user} own ${cfg.directory}"
+        chown -R ${cfg.user}:${cfg.user} ${cfg.directory}
         '';
     };
 
-    users.users.${user} = {
+    users.users.${cfg.user} = {
       isSystemUser = true;
-      home = "${directory}";
-      group = user;
+      home = "${cfg.directory}";
+      group = cfg.user;
       extraGroups = [ "keys" ]; # needed so it can access /run/keys
       createHome = true;
     };
 
-    users.groups.${user} = {};
+    users.groups.${cfg.user} = {};
 
     systemd.services.nginx.serviceConfig.ProtectHome = "read-only";
 
     services = {
       nginx = {
         enable = true;
-        inherit user;
-        group = user;
+        user = cfg.user;
+        group = cfg.user;
+
         virtualHosts.neuron = {
           enableACME = false;
-          root = "${directory}/.neuron/output";
-          locations."/" = {
-            extraConfig = ''
+          root = "${cfg.directory}/.neuron/output";
+          locations."/".extraConfig = ''
               index index.html index.htm;
             '';
-          };
         };
       };
 
       do-on-request = {
-        inherit user;
+        user = cfg.user;
         enable = true;
-        port = actionPort;
-        workingDirectory = "${directory}";
+        port = cfg.refreshPort;
+        workingDirectory = "${cfg.directory}";
         script = ''
           ${gitWithDeployKey} pull
         '';
       };
 
       neuron = {
-        inherit user;
+        user = cfg.user;
         enable = true;
-        path = "${directory}";
+        path = "${cfg.directory}";
       };
     };
-
+  };
   }
