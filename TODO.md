d891c07a351b78669245e0f5ba2dee5f35052af0
Haf <haf@protonmail.ch>
2021-08-15 11:40:18 +0200

    nginx to nix

--- TODO.md
+++ TODO.md
@@ -1,7 +1,6 @@
 # TODO
 * Maybe Zammad? osTicket seems dated
 * A module or library for `PHP` + `nginx`, but I find it difficult since I just copypaste nginx configurations from the internet.
-* I'm making some efforts to get these to work for an installation with multiple modules at the same time, but does it? I've never tested.
 * In `osTicket` and `wallabag`, is there any way I can test that users can login? Maybe I need to automate the browser?
 * I think I've used `mkEnableOption` wrong every single time. It takes a name, not a description.
 * Harden long-standing systemd services (`do-on-request`, `auto-rsync`)
@@ -11,6 +10,10 @@
 
 ## Neuron
 * Finish test. UPDATE: Test that it actually is pulled on a request to `refreshPort`. But how? I can make the request but I don't know how to check it it's pulling. UPDATE: Redirecting the `doOnRequest` log to somewhere and reading it? UPDATE: Is it still necessary? I've tested `doOnRequest` after all. UPDATE: Yeah but these are integration tests and testing the individual components does not guarantee that they all work together
+
+## Multi-machine deploy
+* `nginx` has one user set for each machine and they conflict.
+* Add a way to setup a global certificate, this will help with deploying multiple machines.
 
 ## osTicket
 * Add backup options
@@ -27,7 +30,6 @@
 * Backup options, there's nothing yet but it's pretty important
 * Add email options
 * I think the DB can be authenticated as a Unix socket, check it.
-* `nginx` locations to nix, using `priority`
 
 ## mkInitModule
 * Add an intermediate "lock" for each unit, so these can show at what point of the initialization we are, and thus, restart from an intermediate point instead of from the beginning.
--- machines/wallabag/default.nix
+++ machines/wallabag/default.nix
@@ -30,7 +30,7 @@ let
         default = false;
         description = "Whether to make this user a super-admin";
       };
-    
+
       pocketKeyFile = mkOption {
         type = nullOr (oneOf [str path]);
         default = null;
@@ -91,225 +91,216 @@ in
       ];
 
       machines.wallabag.parameters = {
-          database_driver = "pdo_mysql";
-          database_host = "127.0.0.1";
-          database_port = "~";
-          database_name = cfg.database.name;
-          database_user = cfg.database.user;
-          database_password = myLib.passwd.cat cfg.database.passwordFile;
-          database_path = null;
-          database_table_prefix = cfg.database.prefix;
-          database_socket = null;
-          database_charset = "utf8mb4";
+        database_driver = "pdo_mysql";
+        database_host = "127.0.0.1";
+        database_port = "~";
+        database_name = cfg.database.name;
+        database_user = cfg.database.user;
+        database_password = myLib.passwd.cat cfg.database.passwordFile;
+        database_path = null;
+        database_table_prefix = cfg.database.prefix;
+        database_socket = null;
+        database_charset = "utf8mb4";
 
-          domain_name = if cfg.ssl.enable then "https://localhost" else "http://localhost";
-          server_name = "Your wallabag instance";
+        domain_name = if cfg.ssl.enable then "https://localhost" else "http://localhost";
+        server_name = "Your wallabag instance";
 
-          mailer_transport = "smtp";
-          mailer_user = "~";
-          mailer_password = "~";
-          mailer_host = "127.0.0.1";
-          mailer_port = "false";
-          mailer_encryption = "~";
-          mailer_auth_mode = "~";
+        mailer_transport = "smtp";
+        mailer_user = "~";
+        mailer_password = "~";
+        mailer_host = "127.0.0.1";
+        mailer_port = "false";
+        mailer_encryption = "~";
+        mailer_auth_mode = "~";
 
-          locale = "en";
+        locale = "en";
 
-          secret = "$(${pkgs.libressl}/bin/openssl rand -hex 12)";
+        secret = "$(${pkgs.libressl}/bin/openssl rand -hex 12)";
 
-          twofactor_auth = true;
-          twofactor_sender = "noreply@wallabag.org";
+        twofactor_auth = true;
+        twofactor_sender = "noreply@wallabag.org";
 
-          fosuser_registration = true;
-          fosuser_confirmation = true;
-          fos_oauth_server_access_token_lifetime = 3600;
-          fos_oauth_server_refresh_token_lifetime = 1209600;
-          
-          from_email = "no-reply@wallabag.org";
+        fosuser_registration = true;
+        fosuser_confirmation = true;
+        fos_oauth_server_access_token_lifetime = 3600;
+        fos_oauth_server_refresh_token_lifetime = 1209600;
 
-          rss_limit = 50;
+        from_email = "no-reply@wallabag.org";
 
-          sentry_dsn = "~";
-        } // optionalAttrs (cfg.importTool == "redis") {
-          redis_scheme = "tcp";
-          redis_host = "localhost";
-          redis_port = config.services.redis.port;
-          redis_path = null;
-          redis_password = if (config.services.redis.requirePassFile != null) then myLib.passwd.cat else null;
-        } // optionalAttrs (cfg.importTool == "rabbitmq") {
-          rabbitmq_host = "localhost";
-          rabbitmq_port = config.services.rabbitmq.port;
-          rabbitmq_user = "guest";
-          rabbitmq_password = "guest";
-          rabbitmq_prefetch_count = 10;
-        };
+        rss_limit = 50;
+
+        sentry_dsn = "~";
+      } // optionalAttrs (cfg.importTool == "redis") {
+        redis_scheme = "tcp";
+        redis_host = "localhost";
+        redis_port = config.services.redis.port;
+        redis_path = null;
+        redis_password = if (config.services.redis.requirePassFile != null) then myLib.passwd.cat else null;
+      } // optionalAttrs (cfg.importTool == "rabbitmq") {
+        rabbitmq_host = "localhost";
+        rabbitmq_port = config.services.rabbitmq.port;
+        rabbitmq_user = "guest";
+        rabbitmq_password = "guest";
+        rabbitmq_prefetch_count = 10;
+      };
 
 
 
       networking.firewall.allowedTCPPorts = [ 80 ];
 
-    machines.wallabag.initialization = [
-      {
-        name = "copy-wallabag";
-        description = "Copy wallabag to final directory and setting permissions for installation";
-        script = ''
-          echo '>>> Copying all wallabag files from the store to ${cfg.installation.path}'
-          cp -r ${cfg.package}/* ${cfg.installation.path}
-          echo '>>> Setting write permissions to ${cfg.installation.path}'
-          chmod -R u+w ${cfg.installation.path}
+      machines.wallabag.initialization = [
+        {
+          name = "copy-wallabag";
+          description = "Copy wallabag to final directory and setting permissions for installation";
+          script = ''
+            echo '>>> Copying all wallabag files from the store to ${cfg.installation.path}'
+            cp -r ${cfg.package}/* ${cfg.installation.path}
+            echo '>>> Setting write permissions to ${cfg.installation.path}'
+            chmod -R u+w ${cfg.installation.path}
           '';
-        extraDeps = [ "ensure-paths.service" ];
-      }
+          extraDeps = [ "ensure-paths.service" ];
+        }
 
-      {
-        name = "create-parameters";
-        description = "Create parameters.yml for installation";
-        script =  ''
-          rm -f ${cfg.installation.path}/app/config/parameters.yml
-          echo "${builtins.toJSON { parameters = cfg.parameters; }}" >> ${cfg.installation.path}/app/config/parameters.yml
-            '';
-      }
-
-      {
-        name = "install-wallabag";
-        description = "Install wallabag";
-        script = ''
-          make clean
-          make install
+        {
+          name = "create-parameters";
+          description = "Create parameters.yml for installation";
+          script =  ''
+            rm -f ${cfg.installation.path}/app/config/parameters.yml
+            echo "${builtins.toJSON { parameters = cfg.parameters; }}" >> ${cfg.installation.path}/app/config/parameters.yml
           '';
-        path = with pkgs; [ gnumake bash composerWithTidy phpWithTidy ];
-        extraDeps = [ "setup-wallabag-db.service" ];
-      }
+        }
 
-      {
-        name = "install-dependencies";
-        description = "Run composer install";
-        script = "COMPOSER_MEMORY_LIMIT=-1 composer install || true";
-        path = [ composerWithTidy phpWithTidy ];
-      }
+        {
+          name = "install-wallabag";
+          description = "Install wallabag";
+          script = ''
+            make clean
+            make install
+          '';
+          path = with pkgs; [ gnumake bash composerWithTidy phpWithTidy ];
+          extraDeps = [ "setup-wallabag-db.service" ];
+        }
 
-      {
-        name = "setup-users";
-        description = "Create default users";
-        script = 
-        let
-          insertUser = user: ''
-            php bin/console fos:user:create ${user.username} ${user.email} ${myLib.passwd.cat user.passwordFile} --no-interaction ${optionalString user.superAdmin "--super-admin"}
-            ${optionalString (user.pocketKeyFile != null) (myLib.db.execDML cfg ''
-              SELECT id FROM ${cfg.database.prefix}user WHERE username='${user.username}' INTO @user_id;
-              UPDATE ${cfg.database.prefix}config SET pocket_consumer_key='${myLib.passwd.cat user.pocketKeyFile}' WHERE user_id=@user_id;
+        {
+          name = "install-dependencies";
+          description = "Run composer install";
+          script = "COMPOSER_MEMORY_LIMIT=-1 composer install || true";
+          path = [ composerWithTidy phpWithTidy ];
+        }
+
+        {
+          name = "setup-users";
+          description = "Create default users";
+          script = 
+          let
+            insertUser = user: ''
+              php bin/console fos:user:create ${user.username} ${user.email} ${myLib.passwd.cat user.passwordFile} --no-interaction ${optionalString user.superAdmin "--super-admin"}
+              ${optionalString (user.pocketKeyFile != null) (myLib.db.execDML cfg ''
+                SELECT id FROM ${cfg.database.prefix}user WHERE username='${user.username}' INTO @user_id;
+                UPDATE ${cfg.database.prefix}config SET pocket_consumer_key='${myLib.passwd.cat user.pocketKeyFile}' WHERE user_id=@user_id;
               '')}
-                '';
-        in ''
-          echo '>>> Disabling default "wallabag" user'
-          php bin/console fos:user:deactivate wallabag
-          echo '>>> Creating all users'
-          ${concatStringsSep "\n" (map insertUser cfg.users)}
+              '';
+          in ''
+            echo '>>> Disabling default "wallabag" user'
+            php bin/console fos:user:deactivate wallabag
+            echo '>>> Creating all users'
+            ${concatStringsSep "\n" (map insertUser cfg.users)}
           '';
-        path = [ phpWithTidy ];
-      }
+          path = [ phpWithTidy ];
+        }
 
-      (mkIf (cfg.importTool != "none") {
-        name = "enable-${cfg.importTool}";
-        description = "Enable ${cfg.importTool} for importing in the database";
-        script = myLib.db.execDML cfg "UPDATE ${cfg.database.prefix}internal_setting SET value=1 WHERE name='import_with_${cfg.importTool}';";
-      })
+        (mkIf (cfg.importTool != "none") {
+          name = "enable-${cfg.importTool}";
+          description = "Enable ${cfg.importTool} for importing in the database";
+          script = myLib.db.execDML cfg "UPDATE ${cfg.database.prefix}internal_setting SET value=1 WHERE name='import_with_${cfg.importTool}';";
+        })
 
-    ];
+      ];
 
-    services.nginx = {
-      enable = true;
-      user = mkDefault cfg.installation.user;
-      group = mkDefault cfg.installation.user;
+      services.nginx = {
+        enable = true;
+        user = mkDefault cfg.installation.user;
+        group = mkDefault cfg.installation.user;
 
-      virtualHosts.wallabag = {
-        root = "${cfg.installation.path}/web";
-        extraConfig = ''
-    location / {
-        # try to serve file directly, fallback to app.php
-        try_files $uri /app.php$is_args$args;
-    }
-    location ~ ^/app\.php(/|$) {
-        # if, for some reason, you are still using PHP 5,
-        # then replace /run/php/php7.0 by /var/run/php5
-        fastcgi_pass unix:${config.services.phpfpm.pools.wallabag.socket};
-        fastcgi_split_path_info ^(.+\.php)(/.*)$;
-        include ${pkgs.nginx}/conf/fastcgi_params;
-        include ${pkgs.nginx}/conf/fastcgi.conf;
-        # When you are using symlinks to link the document root to the
-        # current version of your application, you should pass the real
-        # application path instead of the path to the symlink to PHP
-        # FPM.
-        # Otherwise, PHP's OPcache may not properly detect changes to
-        # your PHP files (see https://github.com/zendtech/ZendOptimizerPlus/issues/126
-        # for more information).
-        fastcgi_param  SCRIPT_FILENAME  $realpath_root$fastcgi_script_name;
-        fastcgi_param DOCUMENT_ROOT $realpath_root;
-        # Prevents URIs that include the front controller. This will 404:
-        # http://domain.tld/app.php/some-path
-        # Remove the internal directive to allow URIs like this
-        internal;
-    }
-
-    # return 404 for all other php files not matching the front controller
-    # this prevents access to other php files you don't want to be accessible.
-    location ~ \.php$ {
-        return 404;
-    }
-        '';
-      };
-    };
-
-
-    services.phpfpm = {
-      pools.wallabag = {
-        user = cfg.installation.user;
-        phpPackage = pkgs.php74;
-        settings = {
-          "listen.owner" = config.services.nginx.user;
-          "pm" = "dynamic";
-          "pm.max_children" = 32;
-          "pm.max_requests" = 500;
-          "pm.start_servers" = 2;
-          "pm.min_spare_servers" = 2;
-          "pm.max_spare_servers" = 5;
-          "php_admin_value[error_log]" = "stderr";
-          "php_admin_flag[log_errors]" = true;
-          "catch_workers_output" = true;
+        virtualHosts.wallabag = {
+          root = "${cfg.installation.path}/web";
+          locations = {
+            "/" = {
+              priority = 100;
+              extraConfig = "try_files $uri /app.php$is_args$args;";
+            };
+            "~ ^/app\\.php(/|$)" = {
+              priority = 500;
+              extraConfig = ''
+                fastcgi_pass unix:${config.services.phpfpm.pools.wallabag.socket};
+                fastcgi_split_path_info ^(.+\.php)(/.*)$;
+                include ${pkgs.nginx}/conf/fastcgi_params;
+                include ${pkgs.nginx}/conf/fastcgi.conf;
+                fastcgi_param  SCRIPT_FILENAME  $realpath_root$fastcgi_script_name;
+                fastcgi_param DOCUMENT_ROOT $realpath_root;
+                internal;
+              '';
+            };
+            "~ \\.php$" = {
+              priority = 1000;
+              extraConfig = ''
+                return 404;
+              '';
+            };
+          };
         };
-        phpEnv."PATH" = lib.makeBinPath [ pkgs.php74 ];
       };
-    };
 
-    services.redis = mkIf (cfg.importTool == "redis") {
-      enable = true;
-    };
 
-    services.rabbitmq = mkIf (cfg.importTool == "rabbitmq") {
-      enable = true;
-    };
+      services.phpfpm = {
+        pools.wallabag = {
+          user = cfg.installation.user;
+          phpPackage = pkgs.php74;
+          settings = {
+            "listen.owner" = config.services.nginx.user;
+            "pm" = "dynamic";
+            "pm.max_children" = 32;
+            "pm.max_requests" = 500;
+            "pm.start_servers" = 2;
+            "pm.min_spare_servers" = 2;
+            "pm.max_spare_servers" = 5;
+            "php_admin_value[error_log]" = "stderr";
+            "php_admin_flag[log_errors]" = true;
+            "catch_workers_output" = true;
+          };
+          phpEnv."PATH" = lib.makeBinPath [ pkgs.php74 ];
+        };
+      };
 
-    systemd.services.import-worker = mkIf (cfg.importTool != "none") {
-      description = "Run the worker for asynchronous importing";
+      services.redis = mkIf (cfg.importTool == "redis") {
+        enable = true;
+      };
 
-      after = [ "finish-wallabag-initialization.service" ];
-      requires = [ "${cfg.importTool}.service" ];
-      wantedBy = [ "multi-user.target" ];
+      services.rabbitmq = mkIf (cfg.importTool == "rabbitmq") {
+        enable = true;
+      };
 
-      path = [ phpWithTidy ];
+      systemd.services.import-worker = mkIf (cfg.importTool != "none") {
+        description = "Run the worker for asynchronous importing";
+
+        after = [ "finish-wallabag-initialization.service" ];
+        requires = [ "${cfg.importTool}.service" ];
+        wantedBy = [ "multi-user.target" ];
+
+        path = [ phpWithTidy ];
 
       # If it's not redis, then it's rabbitmq
       script =
-      let
-        command = if (cfg.importTool == "redis") then "wallabag:import:redis-worker" else "rabbitmq:consumer";
-        importer = if (cfg.importTool == "redis") then "pocket" else "import_pocket";
-      in "php bin/console ${command} --env=prod ${importer} -vv";
+        let
+          command = if (cfg.importTool == "redis") then "wallabag:import:redis-worker" else "rabbitmq:consumer";
+          importer = if (cfg.importTool == "redis") then "pocket" else "import_pocket";
+        in "php bin/console ${command} --env=prod ${importer} -vv";
 
-      serviceConfig = {
-        User = cfg.installation.user;
-        WorkingDirectory = cfg.installation.path;
-        Restart = "on-failure";
+        serviceConfig = {
+          User = cfg.installation.user;
+          WorkingDirectory = cfg.installation.path;
+          Restart = "on-failure";
+        };
       };
     };
-  };
-}
+  }
