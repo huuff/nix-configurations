db7d2afbb8facd17ad1be943d4131f8434a58b6a
Haf <haf@protonmail.ch>
2021-08-20 15:18:58 +0200

    inherited vars

--- machines/mail/postfix/testing/virtual-test.nix
+++ machines/mail/postfix/testing/virtual-test.nix
@@ -1,13 +1,12 @@
 { pkgs, ... }:
-# TODO: Some names as in options so I can use inherit??
 let
-  domain = "example.com";
-  user1Address = "user1@${domain}";
-  user2Address = "user2@${domain}";
+  canonicalDomain = "example.com";
+  user1Address = "user1@${canonicalDomain}";
+  user2Address = "user2@${canonicalDomain}";
   installationPath = "/var/lib/postfix";
   testContent = "test content";
   testSubject = "test subject";
-  vmailPath = "/var/lib/vmail";
+  mailPath = "/var/lib/vmail";
 in
 pkgs.nixosTest {
   name = "postfix-virtual";
@@ -21,11 +20,9 @@ pkgs.nixosTest {
 
     machines.postfix = {
       enable = true;
-      canonicalDomain = domain;
+      inherit canonicalDomain mailPath;
 
       maps.virtual_mailbox_maps.path = installationPath;
-
-      mailPath = vmailPath;
 
       users = [
         user1Address
@@ -52,9 +49,9 @@ pkgs.nixosTest {
     with subtest("can send and receive email from local"):
       machine.succeed('echo "${testContent}" | mail -u ${user1Address} -s "${testSubject}" ${user2Address}')
       machine.sleep(1)
-      machine.output_contains('echo p | mail -f ${vmailPath}/${user2Address}/', "To: <${user2Address}>")
-      machine.output_contains('echo p | mail -f ${vmailPath}/${user2Address}/', "Subject: ${testSubject}")
-      machine.output_contains('echo p | mail -f ${vmailPath}/${user2Address}/', "${testContent}")
+      machine.output_contains('echo p | mail -f ${mailPath}/${user2Address}/', "To: <${user2Address}>")
+      machine.output_contains('echo p | mail -f ${mailPath}/${user2Address}/', "Subject: ${testSubject}")
+      machine.output_contains('echo p | mail -f ${mailPath}/${user2Address}/', "${testContent}")
 
   '';
 }
