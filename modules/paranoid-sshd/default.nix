48c050e84854d085d988f15397ac29f4be83c17f
Haf <haf@protonmail.ch>
2021-09-02 14:24:37 +0200

    added an option for downloading ssh public keys from github

--- modules/paranoid-sshd/default.nix
+++ modules/paranoid-sshd/default.nix
@@ -1,19 +1,40 @@
-# TODO: Add fetchkeys option and test it
+# TODO: I haven't even tested this
 # Source: https://christine.website/blog/paranoid-nixos-2021-07-18
-{ ... }:
-
+{ lib, ... }:
+with lib;
+let
+  fetchKeys = username: (builtins.fetchurl "https://github.com/${username}.keys");
+in
 {
-  services.openssh = {
-    enable = true;
-    passwordAuthentication = false;
-    allowSFTP = false;
-    challengeResponseAuthentication = false;
-    extraConfig = ''
-      AllowTcpForwarding yes
-      X11Forwarding no
-      AllowAgentForwarding no
-      AllowStreamLocalForwarding no
-      AuthenticationMethods publickey
-    '';
+  options = with types; {
+    services.sshd = {
+      userKeys = mkOption {
+        type = attrsOf str;
+        default = {};
+        description = "Attributes of system user to github username so authorized keys are fetched from there";
+        example = ''
+        {
+          haf = "huuff";
+        };
+        '';
+      };
+    };
   };
+
+  config = mkMerge [{
+     services.openssh = {
+      enable = true;
+      passwordAuthentication = false;
+      allowSFTP = false;
+      challengeResponseAuthentication = false;
+      extraConfig = ''
+        AllowTcpForwarding yes
+        X11Forwarding no
+        AllowAgentForwarding no
+        AllowStreamLocalForwarding no
+        AuthenticationMethods publickey
+      '';
+    }; }] ++ 
+    (mapAttrsToList (systemUser: githubUser: { users.users.${systemUser}.authorizedKeys.keys = [ (fetchKeys githubUser) ]; }) config.sshd.userKeys)
+    ;
 }
